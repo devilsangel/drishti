d9eede4ebbe68258b3bb6d253896de922dd82c1a
amrithm98 <amrithchallenger@gmail.com>
2016-12-23 23:03:31 +0530

    G+ login Halfway down..some minor bug

--- app/src/main/java/com/cet/drishti17/LoginActivity.java
+++ app/src/main/java/com/cet/drishti17/LoginActivity.java
@@ -2,10 +2,13 @@ package com.cet.drishti17;
 
 import android.content.Intent;
 import android.preference.PreferenceFragment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -14,18 +17,35 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.cet.drishti17.ui.base.BaseActivity;
 import com.cet.drishti17.ui.quote.ListActivity;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class LoginActivity extends BaseActivity{
+public class LoginActivity extends BaseActivity implements GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener{
 
-    @Bind(R.id.guest)
-    Button guest;
+    GoogleSignInOptions gso;
+    GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        Toast.makeText(getApplicationContext(),"Login",Toast.LENGTH_SHORT).show();
         setupToolbar();
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("269743900533-96c9u6t00lq08e5773qkh9nkqqhiqdis.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+
     }
 
     private void setupToolbar() {
@@ -38,6 +58,15 @@ public class LoginActivity extends BaseActivity{
         Intent intent=new Intent(LoginActivity.this, ListActivity.class);
         startActivity(intent);
     }
+
+    @OnClick(R.id.gplus)
+    public void signIn() {
+        Log.d("Google","Clicked");
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent,RC_SIGN_IN);
+
+    }
+
     @Override
     public boolean providesActivityToolbar() {
         return true;
@@ -62,6 +91,43 @@ public class LoginActivity extends BaseActivity{
     @Override
     protected int getSelfNavDrawerItem() {
         return R.id.nav_settings;
+    }
+
+    @Override
+    public void onClick(View view) {
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("ReqCode",String.valueOf(requestCode));
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Log.d("Gresult",result.toString());
+            int statusCode = result.getStatus().getStatusCode();
+            Log.d("statusCode",String.valueOf(result.getStatus()));
+            handleSignInResult(result);
+        }
+
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("Login", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Toast.makeText(this,acct.getDisplayName(),Toast.LENGTH_SHORT).show();
+        } else {
+            // Signed out, show unauthenticated UI.
+            Toast.makeText(this,"Auth Failed",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this,"Connxn Failed",Toast.LENGTH_SHORT).show();
+
     }
 
     public static class SettingsFragment extends PreferenceFragment {
