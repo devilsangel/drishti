fef9209fd89a574a08be735c265687c189adc1cd
amrithm98 <amrithchallenger@gmail.com>
2016-12-24 10:30:29 +0530

    Firebase Added, G+ Fb Login Working

--- app/src/main/java/com/cet/drishti17/LoginActivity.java
+++ app/src/main/java/com/cet/drishti17/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.cet.drishti17;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.preference.PreferenceFragment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,37 +21,80 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.cet.drishti17.ui.base.BaseActivity;
 import com.cet.drishti17.ui.quote.ListActivity;
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends BaseActivity implements GoogleApiClient.OnConnectionFailedListener,
-        View.OnClickListener{
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class LoginActivity extends BaseActivity{
 
     GoogleSignInOptions gso;
     GoogleApiClient mGoogleApiClient;
-    private static final int RC_SIGN_IN = 100;
-
+    private static final int RC_SIGN_IN = 0;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         setupToolbar();
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("269743900533-96c9u6t00lq08e5773qkh9nkqqhiqdis.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this,this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
-                .build();
+        auth=FirebaseAuth.getInstance();
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.cet.drishti17", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("YourKeyHash :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                //System.out.println("YourKeyHash: ", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
 
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
-
+    @OnClick(R.id.logout)
+    public void logOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                           @Override
+                                           public void onComplete(@NonNull Task<Void> task) {
+                                               Toast.makeText(getApplicationContext(), "Logged Out", Toast.LENGTH_SHORT).show();
+                                           }
+                                       }
+                );
+    }
+    @OnClick(R.id.gplus)
+    public void signIn()
+    {
+        if(auth.getCurrentUser()!=null){
+            //user already authenticated
+            Toast.makeText(getApplicationContext(),"Logged In Already",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setProviders(
+                            AuthUI.FACEBOOK_PROVIDER,
+                            AuthUI.GOOGLE_PROVIDER)
+                    .setLogo(R.drawable.p3)
+                    .setTheme(R.style.Base_Theme)
+                    .build(),RC_SIGN_IN);
+        }
+    }
     private void setupToolbar() {
         final ActionBar ab = getActionBarToolbar();
         ab.hide();
@@ -58,15 +105,6 @@ public class LoginActivity extends BaseActivity implements GoogleApiClient.OnCon
         Intent intent=new Intent(LoginActivity.this, ListActivity.class);
         startActivity(intent);
     }
-
-    @OnClick(R.id.gplus)
-    public void signIn() {
-        Log.d("Google","Clicked");
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent,RC_SIGN_IN);
-
-    }
-
     @Override
     public boolean providesActivityToolbar() {
         return true;
@@ -93,41 +131,20 @@ public class LoginActivity extends BaseActivity implements GoogleApiClient.OnCon
         return R.id.nav_settings;
     }
 
-    @Override
-    public void onClick(View view) {
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d("ReqCode",String.valueOf(requestCode));
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            Log.d("Gresult",result.toString());
-            int statusCode = result.getStatus().getStatusCode();
-            Log.d("statusCode",String.valueOf(result.getStatus()));
-            handleSignInResult(result);
+        if(requestCode==RC_SIGN_IN)
+        {
+            if(resultCode==RESULT_OK){
+                //user logged in
+                Log.d("AuthNew",auth.getCurrentUser().getEmail());
+                Intent intent=new Intent(LoginActivity.this,ListActivity.class);
+                intent.putExtra("Data",auth.toString());
+                startActivity(intent);
+            }
         }
-
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d("Login", "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            Toast.makeText(this,acct.getDisplayName(),Toast.LENGTH_SHORT).show();
-        } else {
-            // Signed out, show unauthenticated UI.
-            Toast.makeText(this,"Auth Failed",Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(this,"Connxn Failed",Toast.LENGTH_SHORT).show();
-
     }
 
     public static class SettingsFragment extends PreferenceFragment {
